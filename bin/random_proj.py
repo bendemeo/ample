96038aa530c3babca3e1df44ec798f8345aeb158
Ben DeMeo <bdemeo@mit.edu>
2018-11-29 13:35:20 -0500

    added files

---
+++ bin/random_proj.py
@@ -0,0 +1,95 @@
+# for random projection hashing
+import numpy
+
+# adding stuff for git
+
+class lsh:
+    '''class to construct a random projection of data'''
+
+    def __init__(self, k):
+        ''' k is number of random projections it makes'''
+        ''' dim is number of dimensions '''
+        self.k = k
+        self.hasProjector = False
+        self.hasProjection = False
+        self.hasFinder = False
+
+    def makeProjector(self, dim):
+        self.projector = numpy.random.randn(dim, self.k)
+        self.hasProjector = True
+
+    def project(self, data):  # make a  projection
+        '''data assumed to have one col per dimension
+        and one observation per row'''
+        if not self.hasProjector:
+            self.makeProjector(data.shape[1])
+        self.projection = (numpy.sign(numpy.matmul(data, self.projector))+1)/2
+        self.hasProjection = True
+
+    def getProjection(self, data):  # accessor for projection
+        if not self.hasProjection:
+            self.project(data)
+        return (numpy.sign(numpy.matmul(data, self.projector))+1)/2
+
+    def makeFinder(self, L=5, M=3, data=None):
+        ''' make a list of L dictionaries for randomly chosen subsets
+        of bits'''
+
+        if not (data is None):
+            self.project(data)
+        elif not self.hasProjection:
+            print "can't make hashmaps without data!"
+        dicts = []
+        subsets = []
+        for i in range(L):
+            inds = numpy.random.choice(self.k, M, replace=False)
+            subsets.append(inds)
+            keys = ([tuple(self.projection[row, inds]) for row in range(self.projection.shape[0])])
+            newDict = {}
+            for j in range(len(keys)):
+                k = keys[j]
+                if k in newDict:
+                    # print('key ', k)
+                    # print('value ', newDict[k])
+                    # print('whole dict', newDict)
+                    newDict[k].append(j)
+                else:
+                    newDict[k] = [j]
+            dicts.append(newDict)
+        # print dicts
+        self.finder = dicts
+        self.bands = subsets  # indicies corresponding to 'bands'
+        self.hasFinder = True
+
+    def findCandidates(self, ind, L=None, M=None):
+        if not self.hasFinder:
+            if L is not None and M is not None:
+                self.makeFinder(L, M)
+            else:
+                print "please specify hash size (M) and # of hashes (L)"
+                return(None)
+        candidates = []
+        for i in range(len(self.bands)):
+            band = self.bands[i]
+            d = self.finder[i]
+            key = tuple(self.projection[ind, band])
+            candidates = candidates + d.get(key)  # this is O(n) on gauss
+        #
+        # for d in self.finder:
+        #     for v in list(d.values()):
+        #         # print('value ', v)
+        #         if ind in v:
+        #             candidates = candidates + v
+        return numpy.unique(candidates)
+
+    def downSample(self, k=100):
+        available = range(self.projection.shape[0])
+        sample = []
+        while len(available) > 0 or len(sample) < k:
+            if len(available) == 0:  # reset available if not enough
+                available = range(self.projection.shape[0])
+            next = numpy.random.choice(available)
+            sample.append(next)
+            toRemove = self.findCandidates(next)
+            available = [x for x in available if x not in toRemove]
+        return numpy.unique(sample)
---
+++ bin/test_file.py
@@ -0,0 +1,54 @@
+# for testing stuff
+import numpy
+import random_proj as rp
+import timeit
+import time
+import cProfile
+import matplotlib.pyplot as mpl
+import lsh_sketch
+
+
+def gauss_test(n, d=1, m=1, stdev=[1]):
+    'n points, m gaussias, d dimensions, specified sds'
+    result = numpy.random.randn(1, d)
+    centers = numpy.random.normal(0*d, 10, [m, d])
+    for i in range(len(centers)):
+        print(n)
+        print(d)
+        c = centers[i]
+        result = numpy.concatenate((result,
+                                    numpy.random.normal(c, stdev[i], (n, d))),
+                                    axis = 0)
+    return result
+
+
+if __name__ == '__main__':
+    gauss = numpy.random.randn(100, 99)
+    print(gauss.ndim)
+    scheme = rp.lsh(20)  # make 20 hash functions
+    print scheme.project(gauss)
+    scheme.makeFinder(5, 3)
+    print scheme.findCandidates(1)
+
+    gauss2D = gauss_test(500, 2, 4, [0.1, 1, 0.01, 2])
+    mpl.scatter(gauss2D[:, 0], gauss2D[:, 1])
+
+    subInds = lsh_sketch.lshSketch(gauss2D, 100, numProj=100, numBands=10, bandSize=10)
+
+    # scheme = rp.lsh(200)
+    # scheme.makeFinder(1, 100, data=gauss2D)
+    # subInds = scheme.downSample(sampleSize=20)
+    print(subInds)
+    mpl.scatter(gauss2D[subInds, 0], gauss2D[subInds, 1], c='m')
+    mpl.show()
+
+    # counts = [10000, 20000, 30000, 40000]
+    # for n in counts:
+    #     start = time.time()
+    #     scheme = rp.lsh(20)
+    #     scheme.makeFinder(5, 3, data=gauss_test(n, 2))
+    #     print(scheme.downSample())
+    #     # for i in range(n):
+    #     #     scheme.findCandidates(i)
+    #     print('points: ' + str(n))
+    #     print('time: ' + str(time.time()-start))
