df02ca12948722e33a66f87f562797cf05292092
Ben DeMeo <bdemeo@mit.edu>
2019-01-10 14:01:04 -0500

    test

--- bin/artificial_volume.py
+++ bin/artificial_volume.py
@@ -38,27 +38,123 @@ if __name__ == '__main__':
     expected = np.array([ 1, 1/10., 1/100.])
     expected = np.array(expected) / sum(expected)
 
-    #see how grid size affects this
+    rare_label=2
 
-    filename='gsGridTest_equalvol'
-    iter=1
-    gsGridTestParams = {
-        'opt_grid':[False],
-        'gridSize': np.arange(start=0.99,stop=0.01,step=-0.01).tolist()
+
+    ###geometric sketching###################
+
+    # filename='gsGridTest_equalvol'
+    # iter=1
+    # gsGridTestParams = {
+    #     'opt_grid':[False],
+    #     'gridSize': np.arange(start=1,stop=0.01,step=-0.01).tolist()
+    # }
+    #
+    # gsGridTests = ['max_min_dist','time','kmeans_ami','lastCounts','maxCounts','remnants','rare']
+    #
+    # gsLSH_gridTest = try_params(X_dimred, 'gsLSH',
+    #     params=gsGridTestParams,
+    #     tests=gsGridTests,
+    #     n_seeds=10,
+    #     cell_labels=cell_labels,
+    #     rare_label=rare_label,
+    #     Ns=[100,300,500,800,1000])
+    #
+    # gsLSH_gridTest.to_csv('target/experiments/{}.txt.{}'.format(filename, iter), sep='\t')
+
+
+    ### random grids #################
+    filename='randomGridTest_equalvol'
+    iter = 1
+    randomGridParams = {
+        'gridSize': np.arange(start=1,stop=0.01,step=-0.01).tolist(),
+        'numHashes':[3],
+        'numBands':[1],
+        'bandSize':[3],
     }
 
-    gsGridTests = ['max_min_dist','time','kmeans_ami','lastCounts','maxCounts','remnants','rare', 'gridSize']
+    randomGridTests = ['max_min_dist','time','kmeans_ami','lastCounts','maxCounts','remnants','rare']
 
-    gsLSH_gridTest = try_params(X_dimred, 'gsLSH',
-        params=gsGridTestParams,
-        tests=gsGridTests,
+    randomGridLSH_gridTest = try_params(X_dimred, 'randomGridLSH',
+        params = randomGridParams,
+        tests = randomGridTests,
         n_seeds=10,
         cell_labels=cell_labels,
-        rare_label=2,
+        rare_label=rare_label,
         Ns=[100,300,500,800,1000])
 
+    randomGridLSH_gridTest.to_csv('target/experiments/{}.txt.{}'.format(filename, iter), sep='\t')
 
-    gsLSH_gridTest.to_csv('target/experiments/{}.txt.{}'.format(filename, iter), sep='\t')
+    ######cosine LSH###############
+    filename='cosTest_equalVol'
+    iter = 1
+    cosParams = {
+        'numHashes':np.arange(1,100,1).tolist(),
+        'numBands':[1],
+        'bandSize':np.arange(1,100,1).tolist()
+    }
+
+    cosTests = ['max_min_dist','time','kmeans_ami','lastCounts','maxCounts','remnants','rare']
+
+    cosLSH_test = try_params(X_dimred, 'cosineLSH',
+        params = cosParams,
+        tests = cosTests,
+        n_seeds=10,
+        cell_labels=cell_labels,
+        rare_label=rare_label,
+        Ns=[100,300,500,800,1000])
+
+    cosLSH_test.to_csv('target/experiments/{}.txt.{}'.format(filename, iter), sep='\t')
+
+
+    #### random projection #################
+
+    filename='projTest_equalvol'
+    iter = 1
+    projParams = {
+        'gridSize': np.arange(start=1,stop=0.01,step=-0.01).tolist(),
+        'numHashes':[100],
+        'numBands':[1],
+        'bandSize':[100],
+    }
+
+    projTests = ['max_min_dist','time','kmeans_ami','lastCounts','maxCounts','remnants','rare']
+
+    projLSH_Test = try_params(X_dimred, 'projLSH',
+        params = projParams,
+        tests = projTests,
+        n_seeds=10,
+        cell_labels=cell_labels,
+        rare_label=rare_label,
+        Ns=[100,300,500,800,1000])
+
+    projLSH_Test.to_csv('target/experiments/{}.txt.{}'.format(filename, iter), sep='\t')
+
+
+    ####### random grid, a bunch of them
+    filename='randomGrid_50hash_5band_size5_equalvol'
+    iter = 1
+    gridSizes=np.arange(start=1,stop=0.01,step=-0.01).tolist()
+
+    Params = {
+        'gridSize': gridSizes,
+        'numHashes':[50],
+        'numBands':[5],
+        'bandSize':[5]
+    }
+
+    Tests = ['max_min_dist','time','kmeans_ami','lastCounts','maxCounts','remnants','rare']
+
+    Test = try_params(X_dimred, 'randomGridLSH',
+        params = Params,
+        tests = Tests,
+        n_seeds=10,
+        cell_labels=cell_labels,
+        rare_label=rare_label,
+        Ns=[100,300,500,800,1000])
+
+    Test.to_csv('target/experiments/{}.txt.{}'.format(filename, iter), sep='\t')
+
 
 
 
