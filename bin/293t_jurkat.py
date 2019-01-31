9bcd57826a3e48a97d3eebe4c214b2cbe3feb89a
Ben DeMeo <bdemeo@mit.edu>
2019-01-30 23:22:26 -0500

    dd

--- bin/293t_jurkat.py
+++ bin/293t_jurkat.py
@@ -103,6 +103,7 @@ if __name__ == '__main__':
         print(downsampler)
         downsampler.numCenters=k
         downsampler.makeHash()
+        downsampler.makeFinder()
         downsampler.downsample(100)
         downsampler.vizHash(file='293t_diverseLSH_vizhash_{}'.format(k))
         downsampler.vizSample(file='293t_diverseLSH_vizsample_{}'.format(k))
