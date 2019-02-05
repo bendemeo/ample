28848e28805a95eb9380500d3c462a5ccee626c2
Ben DeMeo <bdemeo@mit.edu>
2019-02-05 15:27:48 -0500

    ss

--- bin/pbmc.py
+++ bin/pbmc.py
@@ -97,7 +97,7 @@ if __name__ == '__main__':
     print(downsampler.sampleEmbedding.shape)
     print(labels.shape)
     plotData = np.concatenate((downsampler.sampleEmbedding, labels), axis=1)
-    plotData = pd.DataFrame(plotData)
+    plotData = pd.DataFrame(plotData, columns = ['x','y','cell_type'])
     plotData.to_csv('pbmc_centerSampler_plotData_5000_100centers', sep='\t')
 
     #
