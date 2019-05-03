5feb8e9c5140ddbfb4776de0ace1fc5045e5bf07
Ben DeMeo <bdemeo@mit.edu>
2019-05-02 21:59:26 -0400

    ss

--- bin/pc_experiments.py
+++ bin/pc_experiments.py
@@ -83,7 +83,8 @@ if __name__ == '__main__':
     filename = 'gsLSH_multigauss_hausdorff'
 
     testParams = {
-        'gridSize':np.arange(1, 0.5, -0.1).tolist()+np.arange(0.4,0.3, -0.05).tolist()+np.arange(0.3, 0.01, -0.01).tolist()
+        'gridSize':np.arange(1, 0.5, -0.1).tolist()+np.arange(0.4,0.3, -0.05).tolist()+np.arange(0.3, 0.01, -0.01).tolist(),
+        'opt_grid': False
     }
     tests = ['time','max_min_dist','occSquares']
 
