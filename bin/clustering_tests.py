395b43ec65e406b41b7bbb227a15fe708ec650b3
Ben DeMeo <bdemeo@mit.edu>
2019-06-28 13:32:17 -0400

    ss

---
+++ bin/clustering_tests.py
@@ -0,0 +1,22 @@
+from fbpca import pca
+import numpy as np
+import os
+from scanorama import *
+from scipy.sparse import vstack
+from sklearn.preprocessing import LabelEncoder, scale
+
+from experiments import *
+from process import load_names
+from utils import *
+from LSH import *
+from hashers import *
+from fttree import *
+import sys
+import pickle
+import scanpy as sc
+
+##PBMC##
+with open('target/experiments/pbmc_ft' as f):
+    order = f.readLines().split('\t')
+
+print(order)
